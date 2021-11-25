9b87a9f185ffa66ddcde376022b5b898a7dd2c6c
AOI_The_RedFox <miho0_0@naver.com>
2021-11-26 07:45:30 +0900

    edit log directory word
    *Manager->manager
    *Agent->agent

--- shelter/installer-v0.6.sh
+++ shelter/installer-v0.6.sh
@@ -3,18 +3,17 @@
 #-----------------------------------------------
 rasp_file="./Agent/armv7_32bit/HurryUp_Agent"
 ubuntu_file="./Agent/ubuntu/HurryUp_Agent"
+rasp_manager_file="./Manager/armv7_32bit/HurryUp_Agent_Manager"
+ubuntu_manager_file="./Manager/ubuntu/HurryUp_Agent_Manager"
+
 check_raspberry_comm=$(uname -a | grep raspberrypi | awk '{print $2}')
 check_ubuntu_comm=$(uname -a | grep Ubuntu | awk '{print $2}')
 
 agent_install_path="/usr/local/bin/"
-agent_install_all_path="/usr/local/bin/$agent_file"
-
-current_dir=$(pwd)
-current_agent_dir="$current_dir/$agent_file"
 
 log_dir_path="/var/log/hurryup/"
-log_manager_path="/var/log/hurryup/Manager/"
-log_and_setupfile_path="/var/log/hurryup/Agent/"
+log_manager_path="/var/log/hurryup/manager/"
+log_and_setupfile_path="/var/log/hurryup/agent/"
 service_file_path="/etc/systemd/system/HurryupAM.service"
 #-----------------------------------------------
 # Simple interface and usage
@@ -59,22 +58,22 @@ function make_service(){
         if [ -f "$service_file_path" ]; then
         echo "[+] Agent service has been already registed. -> $service_file_path"
         else
-        echo "
-[Unit]
+        echo "[Unit]
 Description=BoB10 HurryUp Agent Manager Server
 Wants=network-online.target
 After=network.target network-online.target
 
 [Service]
+Type=forking
+PIDFile=/run/HurryupAM.pid
 ExecStart=/usr/local/bin/HurryUp_Agent_Manager
-WorkingDirectory=/your/working/dir
 Restart=on-failure
 
 [Install]
 WantedBy=multi-user.target " >> /etc/systemd/system/HurryupAM.service
 
 
-        echo "[INFO] Successfully regist Hurryup-AM.service -> $service_file_path"
+        echo "[INFO] Successfully regist HurryupAM.service -> $service_file_path"
         fi
 }
 
@@ -92,7 +91,7 @@ function enable_service(){
                 make_service
                 echo "[INFO] Create Hurryup service."
                 systemctl enable HurryupAM
-                echo "[INFO] Successfully enabl HurryupAM.service."
+                echo "[INFO] Successfully enable HurryupAM.service."
                 systemctl start HurryupAM
                 echo "[INFO] Successfully start HurryupAM.service."
         fi
@@ -101,111 +100,128 @@ function enable_service(){
 
 #-----------------------------------------------
 function install(){
+        # Check Os platform name and load several agent file.
+        if [ $check_raspberry_comm == "raspberrypi" ]; then
+                echo "[INFO] Checking OS platform name.  ->  $check_raspberry_comm"
+                agent_file=$rasp_file
+                manager_file=$rasp_manager_file
+        else
+                if [ $check_ubuntu_comm == "ubuntu" ]; then
+                        echo "[INFO] Checking OS platform name.  ->  $check_ubuntu_comm"
+                        agent_file=$ubuntu_file
+                        manager_file=$ubuntu_manager_file
+                fi
+        fi
 
-	# Check Os platform name and load several agent file.
-	if [ $check_raspberry_comm == "raspberrypi" ]; then
-		echo "[INFO] Checking OS platform name.  ->  $check_raspberry_comm"
-		agent_file=$rasp_file
-	else
-		if [ $check_ubuntu_comm == "ubuntu" ]; then
-			echo "[INFO] Checking OS platform name.  ->  $check_ubuntu_comm"
-			agent_file=$ubuntu_file
-		fi
-	fi
-
-	# Copy Agent file to /usr/local/bin directory
-	if [ -e "$agent_install_all_path" ]; then
-			echo "[+] $agent_file already installed.  ->  $agent_install_path !"
-	else
-			cp -r $current_agent_dir $agent_install_path
-			echo "[INFO] Successfully install $agent_file.  ->   $agent_install_path "
-	fi
+        # Setup agent, manager file directory according to OS platform
+        agent_install_all_path="/usr/local/bin/$agent_file"
+        manager_install_path="/usr/local/bin/$manager_file"
 
 
-	# Check log directory
-	if [ -d "$log_dir_path" ]; then
-			echo "[+] hurryup log directory already created.  -> $log_dir_path"
-	else
-			mkdir $log_dir_path
-			echo "[INFO] Successfully create hurryup log directory.  ->  $log_dir_path"
-			mkdir $log_manager_path
-			echo "[INFO] Successfully create AgentManager log directory.  ->  $log_manager_path"
-			mkdir $log_and_setupfile_path
-			echo "[INFO] Successfully create Agent log directory  ->  $log_and_setupfile_path"
-	fi
+        # Copy Agent file to /usr/local/bin directory
+        if [ -e "$agent_install_all_path" ]; then
+                echo "[+] $agent_file already installed.  ->  $agent_install_path"
+        else
+                cp -r $agent_file $agent_install_path
+                echo "[INFO] Successfully install $agent_file.  ->   $agent_install_path"
+        fi
 
-	# Check service file and execute it.
-	enable_service
+        # Copy Manager file to /usr/local/bin directory
+        if [ -e "$manager_install_path" ]; then
+                echo "[+] $manager_file already installed.  ->  $manager_install_path"
+        else
+                cp -r $manager_file $agent_install_path
+                echo "[INFO] Successfully install $manager_file.  ->  $manager_install_path"
+        fi
+
+
+        # Check log directory
+        if [ -d "$log_dir_path" ]; then
+                        echo "[+] hurryup log directory already created.  -> $log_dir_path"
+        else
+                        mkdir $log_dir_path
+                        echo "[INFO] Successfully create hurryup log directory.  ->  $log_dir_path"
+                        mkdir $log_manager_path
+                        echo "[INFO] Successfully create AgentManager log directory.  ->  $log_manager_path"
+                        mkdir $log_and_setupfile_path
+                        echo "[INFO] Successfully create Agent log directory  ->  $log_and_setupfile_path"
+        fi
+
+        # Check service file and execute it.
+        enable_service
 }
 
 
 #-----------------------------------------------
 function uninstall(){
 
-	echo ""
-	echo "[WARN] This operation might lose all data gnerated of Agent program of the Hurryup solution !!"
-	echo ""
-	echo "Do you uninstall Hurryup Agent program? <y/n> : "
+        echo ""
+        echo "[WARN] This operation might lose all data gnerated of Agent program of the Hurryup solution !!"
+        echo ""
+        echo "Do you uninstall Hurryup Agent program? <y/n> : "
 
-	while true; do
-			read -p "Do you uninstall Hurryup Agent program? <y/n> : " user_answer
-			case user_answer in
-			y)      systemctl disable HurrupAM
-					service_file_path
-					rm -rf $agent_install_path
-					rm -rf $log_dir_path
-					;;
-			n)      exit 1
-					;;
-			*) echo "Please enter your answer 'yes or no' <y/n>"
-					;;
-			esac
-	done
+        while true; do
+                        read -p "Do you uninstall Hurryup Agent program? <y/n> : " user_answer
+                        case user_answer in
+                        y)      systemctl disable HurrupAM
+                                        service_file_path
+                                        rm -rf $agent_install_path
+                                        rm -rf $log_dir_path
+                                        ;;
+                        n)      exit 1
+                                        ;;
+                        *) echo "Please enter your answer 'yes or no' <y/n>"
+                                        ;;
+                        esac
+        done
 
-	#inform about delete
-	echo "[INFO] Successfully disable system service.  ->  $service_file_path"
-	echo "[INFO] Successfully delete system service file.  ->  $HurryupAM.service"
-	echo "[INFO] Successfully delete Hurryup agent file.  ->  $agent_install_all_path"
-	echo "[INFO] Successfully delete Hurryup log files.
-					->      $log_manager_path
-							$log_manager_path
-							$log_dir_path"
+        #inform about delete
+        echo "[INFO] Successfully disable system service.  ->  $service_file_path"
+        echo "[INFO] Successfully delete system service file.  ->  $HurryupAM.service"
+        echo "[INFO] Successfully delete Hurryup agent file.  ->  $agent_install_all_path"
+        echo "[INFO] Successfully delete Hurryup log files.
+                                        ->      $log_manager_path
+                                                        $log_manager_path
+                                                        $log_dir_path"
 
-	echo ""
-	echo "[INFO] Complete deleted Hurryup Agent program."
-	exit 1
+        echo ""
+        echo "[INFO] Complete deleted Hurryup Agent program."
+        exit 1
 }
 
 
 #-----------------------------------------------
 function disable_servise(){
-	if [ -f "$service_file_path" ]; then
-			systemctl disable HurryupAM
-			echo "[INFO] Successfully disable HurryupAM.service."
-	else
-			echo "[WARN] HurryupAM.service not found !"
-	fi
+        if [ -f "$service_file_path" ]; then
+                systemctl stop HurryupAM
+                echo "[INFO] Successfully stop HurryupAM.service."
+                systemctl disable HurryupAM
+                echo "[INFO] Successfully disable HurryupAM.service."
+        else
+                echo "[WARN] HurryupAM.service not found !"
+        fi
 }
 
 
 #-----------------------------------------------
 while getopts hiued opts; do
-	case $opts in
-	h) usage
-			;;
-	\?) usage
-			;;
-	i) install
-			;;
-	u) uninstall
-			;;
-	e) enable_service
-			;;
-	d) disable_servise
-			;;
+        case $opts in
+        h) usage
+                        ;;
+        \?) usage
+                        ;;
+        i) install
+                        ;;
+        u) uninstall
+                        ;;
+        e) enable_service
+                        ;;
+        d) disable_servise
+                        ;;
 
-	esac
+        esac
 done
 
 
 #-----------------------------------------------
+~
